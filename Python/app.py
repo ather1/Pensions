2d9d181b92524d3814048477815ad03110d6c66b
ather1 <mohammed_ather@hotmail.com>
2019-06-07 05:43:10 +0100

    url_for

--- Python/app.py
+++ Python/app.py
@@ -5,8 +5,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    now = datetime.datetime.now()
-    new_year = now.day==25 and now.month ==5
-    # text = "Yes"  if  new_year  else "No"
-    return  render_template("index.html",new_year = new_year)
+    return  render_template("index.html")
+    
+@app.route("/more")
+def  more():
+    return render_template("more.html")
     
