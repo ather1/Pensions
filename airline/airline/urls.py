8b71fd94ba3402b4c350efe591e2953d9d732aeb
ather1 <mohammed_ather@hotmail.com>
2019-07-03 10:53:20 +0100

    Lesson 7

--- airline/airline/urls.py
+++ airline/airline/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 
+
 urlpatterns = [
     path('',include('flights.urls')),
     path('admin/', admin.site.urls),
--- airline/flights/views.py
+++ airline/flights/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404, HttpResponseRedirect
+from .models import Flight, Passenger
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello to AIRLINE !")
+    context = {
+        "flights" : Flight.objects.all()}
+    return  render(request,"flights/index.html",context)
 
+def  flight(request,flight_id):
+    try:
+        flight = Flight.objects.get(pk=flight_id)
+    except Flight.DoesNotExist:
+        raise Http404("Flight does not exist")
+    context = {
+        "flight":flight,
+        "passengers":flight.passengers.all(),
+        "non_passengers":Passenger.objects.exclude(flights=flight).all()
+    }
+    return render(request,"flights/flight.html",context)
 
-
-
+def book(request,flight_id):
+    try:
+        passenger_id = int(request.POST["passenger"])
+        passenger = Passenger.objects.get(pk=passenger_id)
+        flight = Flight.objects.get(pk=flight_id)
+    except Flight.DoesNotExist:
+        return render (request,"flights/error.html",{"No Flight found"})
+    except Passenger.DoesNotExist:
+        return render(request , "flights/error.html",{"No Passendger found"})
+    except KeyError:
+        return render (request , "flights/error.html", {"No passenger selected"})
+    
+    passenger.flights.add(flight) 
+    return HttpResponseRedirect(reverse("flight",args=(flight_id,)))
+    
